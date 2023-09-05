cbf23222808bf13c2c0f1e4b33da694b24e62543
Zal-atan <ethanzalta@yahoo.com>
2023-09-05 15:45:01 -0700

    quick fix

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -3,4 +3,4 @@ for character in range(97, 123):
     if chr(character) == "e" or chr(character) == "q":
         continue
     else:
-        print("{:c}".format(character), end= "")
+        print("{:c}".format(character), end="")

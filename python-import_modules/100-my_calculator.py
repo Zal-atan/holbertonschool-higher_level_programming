8d197bf3397a45f306a84c241c9a6e50abf052c1
Zal-atan <ethanzalta@yahoo.com>
2023-09-06 09:54:22 -0700

    On calculator trying different exit statuses

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -5,12 +5,12 @@ from sys import argv
 operators = ("+", "-", "*", "/")
 # if __name__ == "__main__":
 if len(argv) != 4:
-    print("1")
-    exit()
+    # print("1")
+    exit(1)
 if argv[2] not in operators:
     print("Unknown operator. Available operators: +, -, * and /")
-    print("1")
-    exit()
+    # print("1")
+    exit(1)
 a = int(argv[1])
 b = int(argv[3])
 if argv[2] == "+":
@@ -21,3 +21,5 @@ elif argv[2] == "*":
     print("{} * {} = {}".format(a, b, mul(a, b)))
 elif argv[2] == "/":
     print("{} / {} = {}".format(a, b, div(a,b)))
+
+exit(0)

0a1a341d3613161e0de663442dc94057c758a219
Zal-atan <ethanzalta@yahoo.com>
2023-09-06 09:43:52 -0700

    Test on 100, my calculator

---
+++ python-import_modules/100-my_calculator.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+from sys import argv
+
+operators = ("+", "-", "*", "/")
+if __name__ == "__main__":
+    if len(argv) != 4:
+        print("1")
+        exit()
+    if argv[2] not in operators:
+        print("Unknown operator. Available operators: +, -, * and /")
+        print("1")
+        exit()
+    a = int(argv[1])
+    b = int(argv[3])
+    if argv[2] == "+":
+        print("{} + {} = {}".format(a, b, add(a,b)))
+    elif argv[2] == "-":
+        print("{} - {} = {}".format(a, b, sub(a,b)))
+    elif argv[2] == "*":
+        print("{} * {} = {}".format(a, b, mul(a, b)))
+    elif argv[2] == "/":
+        print("{} / {} = {}".format(a, b, div(a,b)))

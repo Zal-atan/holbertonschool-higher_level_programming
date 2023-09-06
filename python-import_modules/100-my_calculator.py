8fb3faf99b4d7422382d3930e32f2dffdf0dd3ee
Zal-atan <ethanzalta@yahoo.com>
2023-09-06 09:54:56 -0700

    quick fix

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -3,23 +3,23 @@ from calculator_1 import add, sub, mul, div
 from sys import argv
 
 operators = ("+", "-", "*", "/")
-# if __name__ == "__main__":
-if len(argv) != 4:
-    # print("1")
-    exit(1)
-if argv[2] not in operators:
-    print("Unknown operator. Available operators: +, -, * and /")
-    # print("1")
-    exit(1)
-a = int(argv[1])
-b = int(argv[3])
-if argv[2] == "+":
-    print("{} + {} = {}".format(a, b, add(a,b)))
-elif argv[2] == "-":
-    print("{} - {} = {}".format(a, b, sub(a,b)))
-elif argv[2] == "*":
-    print("{} * {} = {}".format(a, b, mul(a, b)))
-elif argv[2] == "/":
-    print("{} / {} = {}".format(a, b, div(a,b)))
+if __name__ == "__main__":
+    if len(argv) != 4:
+        # print("1")
+        exit(1)
+    if argv[2] not in operators:
+        print("Unknown operator. Available operators: +, -, * and /")
+        # print("1")
+        exit(1)
+    a = int(argv[1])
+    b = int(argv[3])
+    if argv[2] == "+":
+        print("{} + {} = {}".format(a, b, add(a,b)))
+    elif argv[2] == "-":
+        print("{} - {} = {}".format(a, b, sub(a,b)))
+    elif argv[2] == "*":
+        print("{} * {} = {}".format(a, b, mul(a, b)))
+    elif argv[2] == "/":
+        print("{} / {} = {}".format(a, b, div(a,b)))
 
-exit(0)
+    exit(0)

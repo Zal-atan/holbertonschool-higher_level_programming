626454157fbbc71f7a7d09178753c5b3d5dba78b
Zal-atan <ethanzalta@yahoo.com>
2023-09-06 05:54:34 -0700

    Adding file 3 - infinite print

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+from sys import argv
+
+if __name__ == "__main__":
+    args = len(argv)
+    sum = 0
+    if args == 1:
+        print("0")
+    elif args == 2:
+        print("{}".format(argv[1]))
+    else:
+        for i in range(1, args):
+            sum += int(argv[i])
+        print("{}".format(sum))

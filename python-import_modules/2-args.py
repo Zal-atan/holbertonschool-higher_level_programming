c89c8e79722dbc28d3599263d2cbe7a761323468
Zal-atan <ethanzalta@yahoo.com>
2023-09-06 05:43:51 -0700

    Adding file 2-args

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+from sys import argv
+
+if __name__ == "__main__":
+    args = len(argv)
+    if args == 1:
+        print(("0 arguments"))
+    else:
+        print("{} {}:".format(args - 1,
+                              "argument" if args == 2 else "arguments"))
+    for i in range(1, args):
+        print("{}: {}".format(i, argv[i]))

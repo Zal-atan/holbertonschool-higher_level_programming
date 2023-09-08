ba8e1ae525fd1f6408f4736265521c7af9fb4cdb
Zal-atan <ethanzalta@yahoo.com>
2023-09-08 12:36:16 -0700

    Taking out the test on 0

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -13,13 +13,3 @@ def square_matrix_simple(matrix=[]):
         new_matrix.append(new_list)
 
     return new_matrix
-
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
-
-new_matrix = square_matrix_simple(matrix)
-print(new_matrix)
-print(matrix)

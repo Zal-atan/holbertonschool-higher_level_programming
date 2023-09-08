aaf616c50e1678ce85a1a6b8c25ec588e130e484
Zal-atan <ethanzalta@yahoo.com>
2023-09-08 14:31:42 -0700

    Adding base structure to number 12 roman-to-int

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def roman_to_int(roman_string):
+    rom_dict = {"M": 1000, "D": 500, "C": 100, "L": 50, "X": 10, "V": 5, "I": 1}
+    len_str = len(roman_string)
+    sum = 0
+    for i in range (0, len_str):
+        if

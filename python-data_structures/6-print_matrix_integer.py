e8ec70eaaec716f0699bb0e2f1698c78086d6cf2
Zal-atan <ethanzalta@yahoo.com>
2023-09-08 09:57:54 -0700

    Fixing pycodestyle error on 6

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -6,6 +6,6 @@ def print_matrix_integer(matrix=[[]]):
         if listLen == 0:
             print()
             continue
-        for num in range (0, listLen - 1):
+        for num in range(0, listLen - 1):
             print("{:d}".format(list[num]), end=" ")
         print("{:d}".format(list[listLen - 1]))

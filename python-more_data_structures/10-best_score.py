949a5ff680477f006a849ff5d1389adcc5310b11
Zal-atan <ethanzalta@yahoo.com>
2023-09-08 14:05:44 -0700

    Forgot to t ake out test on 10

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -12,11 +12,3 @@ def best_score(a_dictionary):
     if max_score == 0:
         return None
     return highest
-
-
-a_dictionary = {'John': 12, 'Bob': 14, 'Mike': 14, 'Molly': 16, 'Adam': 10}
-best_key = best_score(a_dictionary)
-print("Best score: {}".format(best_key))
-
-best_key = best_score(None)
-print("Best score: {}".format(best_key))

f1ea58e00328f085ae55e7fbcfd2b031a8bbea2d
Zal-atan <ethanzalta@yahoo.com>
2023-09-08 12:02:46 -0700

    Next try of 11-delete_at

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -4,20 +4,6 @@ def delete_at(my_list=[], idx=0):
     listLen = len(my_list)
     if listLen <= idx or idx < 0:
         return my_list
-    new_list = []
-    i = 0
-    for num in range(0, listLen):
-        if i == idx:
-            i += 1
-            continue
-        new_list.append(my_list[num])
-        i += 1
+    del my_list[idx]
 
-    my_list = new_list
-    return new_list
-
-my_list = [1, 2, 3, 4, 5]
-idx = 3
-new_list = delete_at(my_list, idx)
-print(new_list)
-print(my_list)
+    return my_list

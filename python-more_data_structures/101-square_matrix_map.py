c12eda30bdc41ba3d820d2035448a7a8b36efad1
Zal-atan <ethanzalta@yahoo.com>
2023-09-09 14:07:08 -0700

    Forgot to take out test

--- python-more_data_structures/101-square_matrix_map.py
+++ python-more_data_structures/101-square_matrix_map.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
     return list(map(lambda row: list(map(lambda x: x ** 2, row)), matrix))
-
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]

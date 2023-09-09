82420070e814bc75c05d159c898e6b056702bca7
Zal-atan <ethanzalta@yahoo.com>
2023-09-09 09:04:49 -0700

    Trying to fix pycode errors

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,24 +1,33 @@
 #!/usr/bin/python3
 
 def roman_to_int(roman_string):
-    rom_dict = {"M": 1000, "D": 500, "C": 100, "L": 50, "X": 10, "V": 5, "I": 1}
+    rom_dict = {"M": 1000, "D": 500, "C": 100,
+                "L": 50, "X": 10, "V": 5, "I": 1}
     len_str = len(roman_string)
-    if roman_string == None or len_str < 1 or type(roman_string) != str:
+    if roman_string is None or len_str < 1 or type(roman_string) != str:
         return 0
     sum = 0
     i = 0
     while(i < len_str):
-        if i < (len_str - 1) and rom_dict[roman_string[i]] < rom_dict[roman_string[i + 1]]:
-            if (roman_string[i] == "I") and (roman_string[i + 1] == "V" or roman_string[i + 1] == "X"):
-                sum += (rom_dict[roman_string[i + 1]] - rom_dict[roman_string[i]])
+        if i < (len_str - 1) and (rom_dict[roman_string[i]] <
+                                  rom_dict[roman_string[i + 1]]):
+            if (roman_string[i] == "I") and (roman_string[i + 1] == "V" or
+                                             roman_string[i + 1] == "X"):
+                sum += (rom_dict[roman_string[i + 1]] -
+                        rom_dict[roman_string[i]])
                 i += 2
-            elif (roman_string[i] == "X") and (roman_string[i + 1] == "L" or roman_string[i + 1] == "C"):
-                sum += (rom_dict[roman_string[i + 1]] - rom_dict[roman_string[i]])
+            elif (roman_string[i] == "X") and (roman_string[i + 1] == "L" or
+                                               roman_string[i + 1] == "C"):
+                sum += (rom_dict[roman_string[i + 1]] -
+                        rom_dict[roman_string[i]])
                 i += 2
-            elif (roman_string[i] == "C") and (roman_string[i + 1] == "" or roman_string[i + 1] == "M"):
-                sum += (rom_dict[roman_string[i + 1]] - rom_dict[roman_string[i]])
+            elif (roman_string[i] == "C") and (roman_string[i + 1] == "" or
+                                               roman_string[i + 1] == "M"):
+                sum += (rom_dict[roman_string[i + 1]] -
+                        rom_dict[roman_string[i]])
                 i += 2
-            else: return 0
+            else:
+                return 0
         else:
             sum += rom_dict[roman_string[i]]
             i += 1

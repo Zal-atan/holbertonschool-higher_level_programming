407483f1c0d3dfd32eec1f22bd417bc86a756291
Zal-atan <ethanzalta@yahoo.com>
2023-09-09 14:00:49 -0700

    First try on bonus 100-weight_average

---
+++ python-more_data_structures/100-weight_average.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def weight_average(my_list=[]):
+    if my_list is None:
+        return 0
+    denominator = 0
+    numerator = 0
+    for pair in my_list:
+        denominator += pair[1]
+        numerator += (pair[0] * pair[1])
+
+    return numerator/denominator

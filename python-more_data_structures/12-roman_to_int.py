77844986483fb73f8d43f93741c419bdafae3482
Zal-atan <ethanzalta@yahoo.com>
2023-09-09 08:15:26 -0700

    First real working version of 12- roman numerals

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,16 @@
 
 def roman_to_int(roman_string):
     rom_dict = {"M": 1000, "D": 500, "C": 100, "L": 50, "X": 10, "V": 5, "I": 1}
+    if roman_string == None:
+        return None
     len_str = len(roman_string)
     sum = 0
-    for i in range (0, len_str):
-        if
+    i = 0
+    while(i < len_str):
+        if i < (len_str - 1) and rom_dict[roman_string[i]] < rom_dict[roman_string[i + 1]]:
+            sum += (rom_dict[roman_string[i + 1]] - rom_dict[roman_string[i]])
+            i += 2
+        else:
+            sum += rom_dict[roman_string[i]]
+            i += 1
+    return sum

a561ce41c17c6a0763706d581f5f9e03ff508773
Zal-atan <ethanzalta@yahoo.com>
2023-09-09 13:39:34 -0700

    removing else statement

--- python-exceptions/102-magic_calculation.py
+++ python-exceptions/102-magic_calculation.py
@@ -6,8 +6,7 @@ def magic_calculation(a, b):
         try:
             if i > a:
                 raise Exception('Too far')
-            else:
-                result += (a ** b) / i
+            result += (a ** b) / i
         except Exception as e:
             result = a + b
             break

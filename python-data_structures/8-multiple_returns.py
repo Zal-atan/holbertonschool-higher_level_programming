ae0137afbf4edd3ac5f7fb5c520e410a78da8ff7
Zal-atan <ethanzalta@yahoo.com>
2023-09-09 13:48:45 -0700

    Taking that back and trying another return

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 
 def multiple_returns(sentence):
-    if sentence is None:
-        return None
     lenSen = len(sentence)
     if lenSen == 0:
-        return None, None
+        return 0, None
     return lenSen, sentence[0]

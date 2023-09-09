91627ccd259ceea2ce94d4a586d9b156b25d0c15
Zal-atan <ethanzalta@yahoo.com>
2023-09-09 14:14:08 -0700

    removing test on 102

--- python-more_data_structures/102-complex_delete.py
+++ python-more_data_structures/102-complex_delete.py
@@ -1,11 +1,5 @@
 #!/usr/bin/python3
 
-def print_sorted_dictionary(a_dictionary):
-    sorted_dict = dict(sorted(a_dictionary.items()))
-    for key in sorted_dict:
-        print("{}: {}".format(key, sorted_dict[key]))
-
-
 def complex_delete(a_dictionary, value):
     list_of_keys = []
     for key in a_dictionary:
@@ -14,16 +8,3 @@ def complex_delete(a_dictionary, value):
     for key in list_of_keys:
         del a_dictionary[key]
     return a_dictionary
-
-a_dictionary = {'lang': "C", 'track': "Low", 'pref': "C", 'ids': [1, 2, 3]}
-new_dict = complex_delete(a_dictionary, 'C')
-print_sorted_dictionary(a_dictionary)
-print("--")
-print_sorted_dictionary(new_dict)
-
-print("--")
-print("--")
-new_dict = complex_delete(a_dictionary, 'Low')
-print_sorted_dictionary(a_dictionary)
-print("--")
-print_sorted_dictionary(new_dict)

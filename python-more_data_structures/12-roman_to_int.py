f5c8336d67db3037cbdd38877c57102ea782f232
Zal-atan <ethanzalta@yahoo.com>
2023-09-12 09:11:00 -0700

    Trying to take out if len_str

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -3,9 +3,9 @@
 def roman_to_int(roman_string):
     rom_dict = {"M": 1000, "D": 500, "C": 100,
                 "L": 50, "X": 10, "V": 5, "I": 1}
+    if roman_string is None or type(roman_string) != str:
+        return 0
     len_str = len(roman_string)
-    if roman_string is None or len_str < 1 or type(roman_string) != str:
-        return int(0)
     sum = 0
     i = 0
     while(i < len_str):

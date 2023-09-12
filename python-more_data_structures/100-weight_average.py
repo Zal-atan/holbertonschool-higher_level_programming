d559e5234816b19ea7ff1a75184182c17b444e67
Zal-atan <ethanzalta@yahoo.com>
2023-09-12 09:54:30 -0700

    Trying a final try for 13

--- python-more_data_structures/100-weight_average.py
+++ python-more_data_structures/100-weight_average.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def weight_average(my_list=[]):
-    if my_list is None:
+    if my_list is None or type(my_list) != list:
         return 0
     denominator = 0
     numerator = 0

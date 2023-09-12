76807b9b1c118db82938ed15c210b6b70c20c792
Zal-atan <ethanzalta@yahoo.com>
2023-09-12 09:57:07 -0700

    Adding in one more 0 return to 13

--- python-more_data_structures/100-weight_average.py
+++ python-more_data_structures/100-weight_average.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def weight_average(my_list=[]):
-    if my_list is None or type(my_list) != list:
+    if my_list is None or type(my_list) != list or len(my_list) == 0:
         return 0
     denominator = 0
     numerator = 0

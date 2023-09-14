4862629551e99bf31a1cc7af056d78e9f5b92d41
Zal-atan <ethanzalta@yahoo.com>
2023-09-14 12:56:16 -0700

    Trying more small fixes, pycodestle

--- python-classes/6-main.py
+++ python-classes/6-main.py
@@ -15,6 +15,11 @@ my_square_2.my_print()
 print("--")
 
 my_square_3 = Square(3, (3, 0))
+print(f"Position: {my_square_3.position}")
+print(f"PositionX: {my_square_3.position[0]}")
+my_square_3.position = (4, 3)
+print(f"Position: {my_square_3.position}")
+print(f"PositionX: {my_square_3.position[0]}")
 my_square_3.my_print()
 
 print("--")
--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -34,13 +34,11 @@ class Square:
     @position.setter
     def position(self, value):
         """Sets a new value for position -- Setter"""
-        if type(value) is not tuple:
+        if type(value) is not tuple or len(value) != 2:
             raise TypeError("position must be a tuple of 2 positive integers")
         if type(value[0]) is not int or type(value[1]) is not int:
             raise TypeError("position must be a tuple of 2 positive integers")
-        if value[0] < 0 or value [1] < 0:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        if len(value) > 2:
+        if value[0] < 0 or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 

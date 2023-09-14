c16dfd3da1d06448f5d43b82c3255971d2cf37c3
Zal-atan <ethanzalta@yahoo.com>
2023-09-14 11:27:39 -0700

    Adding file 2-square

---
+++ python-classes/2-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+# 0-square.py
+# Ethan Zalta
+"""This is a file creating a class for Square."""
+
+
+class Square:
+    """Currently an empty class to define square"""
+
+    def __init__(self, size=0):
+        """Now Initializing Square with a size"""
+        self.__size = size
+
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")

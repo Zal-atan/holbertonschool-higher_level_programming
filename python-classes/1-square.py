a767bfcd2b46e80eac6c6a717d138f5c0d65d940
Zal-atan <ethanzalta@yahoo.com>
2023-09-14 11:21:52 -0700

    Adding file 1-square

---
+++ python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# 0-square.py
+# Ethan Zalta
+"""This is a file creating a class for Square."""
+
+
+class Square:
+    """Currently an empty class to define square"""
+
+    def __init__(self, size):
+        """Now Initializing Square with a size"""
+        self.__size = size

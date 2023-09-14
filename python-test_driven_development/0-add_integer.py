80126488a8747d16d0e53c6d8c9460a9392eb8b0
Zal-atan <ethanzalta@yahoo.com>
2023-09-14 15:53:48 -0700

    Tests passing for all integers and floats

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -4,4 +4,8 @@
 """This is a function for adding two integers or floats"""
 
 def add_integer(a, b=98):
+    if type(a) is float:
+        a = int(a)
+    if type(b) is float:
+        b = int(b)
     return (a + b)

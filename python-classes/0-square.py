6a126b7de250abfbe7b0cc4e2ad32c9b4e51cb27
Zal-atan <ethanzalta@yahoo.com>
2023-09-14 11:18:42 -0700

    Putting a second line between top and class definition

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 # 0-square.py
 # Ethan Zalta
-"""This is a file creating a class for Square"""
+"""This is a file creating a class for Square."""
+
 
 class Square:
-    """Class Defining a Square"""
+    """Currently an empty class to define square"""
 
     pass

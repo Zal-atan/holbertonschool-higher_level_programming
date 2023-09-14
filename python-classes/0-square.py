880ba72c77ea42cc9365c8013f085493f901094f
Zal-atan <ethanzalta@yahoo.com>
2023-09-14 11:12:03 -0700

    Trying to add documentation to file 0 to see if it helps

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
+"""This is a file creating a class for Square"""
 
 class Square():
     """Class Defining a Square"""
 
-    def __init__(self) -> None:
+    def __init__(self, size=None) -> None:
         """Initializing the square"""
-        self.size = None
+        pass

66c07db6f26650150345a43cf46c3ac8ac0975c1
Zal-atan <ethanzalta@yahoo.com>
2023-09-15 11:52:50 -0700

    Adding test files for 4-print_square

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# 0-add_integer.py
+# Ethan Zalta
+"""TThis is a function for printing a square"""
+
+
+def print_square(size):
+    """ This function should print a square with length and width equal to
+    input size.
+    Size must be an integer of greater than or equal to 0"""
+

5aba9952545a15c7fd594db0011a249a277b8929
Zal-atan <ethanzalta@yahoo.com>
2023-09-15 14:31:26 -0700

    Adding functionality with most Correct Inputs

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -7,3 +7,15 @@
 def text_indentation(text):
     """ This function prints a given text, and separates all instances of . and
     ? and : by two new lines each time. This function only accepts strings"""
+    counter = 0
+    for character in text:
+        if character == ":" or character == "." or character == "?":
+            print(character)
+            print()
+            counter = 0
+        elif counter == 0 and character == " ":
+            counter += 1
+            continue
+        else:
+            print(character, end="")
+            counter += 1

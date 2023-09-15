b2ff1a11598cb4cba4330caf74111c67404d0582
Zal-atan <ethanzalta@yahoo.com>
2023-09-15 10:15:45 -0700

    Had to change tests, thought float should always be 2, even with 0's, first version working without Errors

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -2,3 +2,10 @@
 # 0-add_integer.py
 # Ethan Zalta
 """This is a function for adding two integers or floats"""
+
+
+def matrix_divided(matrix, div):
+    """ This function divides an input matrix by a given integer or float (div)
+    Each list in the matrix must only contain integers and floats and must be
+    of equal size. Can not divide by 0"""
+    return [[round(x / div, 2) for x in row] for row in matrix]

0f926e29d56cb64b9d34b755f8c4d831416d7cfd
Zal-atan <ethanzalta@yahoo.com>
2023-09-15 14:42:28 -0700

    Works for all normal cases now

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -8,14 +8,19 @@ def text_indentation(text):
     """ This function prints a given text, and separates all instances of . and
     ? and : by two new lines each time. This function only accepts strings"""
     counter = 0
+    textlen = 0
     for character in text:
         if character == ":" or character == "." or character == "?":
             print(character)
             print()
             counter = 0
+            textlen += 1
         elif counter == 0 and character == " ":
             counter += 1
+            continue
+        elif textlen == (len(text) - 1) and character == " ":
             continue
         else:
             print(character, end="")
             counter += 1
+        textlen += 1

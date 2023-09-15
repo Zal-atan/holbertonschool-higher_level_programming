223c78b6e8c579f2245652d91aecfdb75109bea2
Zal-atan <ethanzalta@yahoo.com>
2023-09-15 11:57:59 -0700

    All basic tests passed

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -8,6 +8,10 @@ def print_square(size):
     """ This function should print a square with length and width equal to
     input size.
     Size must be an integer of greater than or equal to 0"""
+    if type(size) is not int:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
     for i in range(0, size):
         for i in range(0, size):
             print("#", end="")

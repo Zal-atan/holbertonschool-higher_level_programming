f412a0bbd391470b6569f64a49809a15f86c2e55
Zal-atan <ethanzalta@yahoo.com>
2023-09-15 11:54:22 -0700

    Basic tests passed, now for exceptions

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -8,4 +8,8 @@ def print_square(size):
     """ This function should print a square with length and width equal to
     input size.
     Size must be an integer of greater than or equal to 0"""
+    for i in range(0, size):
+        for i in range(0, size):
+            print("#", end="")
+        print()
 

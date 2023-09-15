016258262851d95df82e44002ab45fd357e53ecc
Zal-atan <ethanzalta@yahoo.com>
2023-09-15 14:19:58 -0700

    Adding test files for 5-text_indentation

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# 0-add_integer.py
+# Ethan Zalta
+"""This is a function for Printing and separating a given Text"""
+
+
+def text_indentation(text):
+    """ This function prints a given text, and separates all instances of . and
+    ? and : by two new lines each time. This function only accepts strings"""

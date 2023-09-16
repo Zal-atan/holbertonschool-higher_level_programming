71ebf9e4bebfdac2032c892d2346a50ad3dfb500
Zal-atan <ethanzalta@yahoo.com>
2023-09-16 09:34:00 -0700

    Adding new directory - more classes, adding README, andding file 0

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# 0-rectangle.py
+# Ethan Zalta
+"""This is a file creating a class for Rectangle."""
+
+
+class Rectangle:
+    """Currently an empty class to define Rectangle"""
+
+    pass

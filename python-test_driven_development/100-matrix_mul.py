b4bd32d7d9523e66a471a4072e9075b00b407db8
Zal-atan <ethanzalta@yahoo.com>
2023-09-19 11:49:10 -0700

    Most tests finished for 100-matrix-mul

--- python-test_driven_development/100-matrix_mul.py
+++ python-test_driven_development/100-matrix_mul.py
@@ -6,6 +6,38 @@
 
 def matrix_mul(m_a, m_b):
     """This function multiplies matrix_a by matrix_b"""
+    if type(m_a) is not list:
+        raise TypeError("m_a must be a list")
+    if type(m_b) is not list:
+        raise TypeError("m_b must be a list")
+    for innerList in m_a:
+        if type(innerList) is not list:
+            raise TypeError("m_a must be a list of lists")
+    for innerList in m_b:
+        if type(innerList) is not list:
+            raise TypeError("m_b must be a list of lists")
+    for innerList in m_a:
+        if innerList == []:
+            raise TypeError("m_a can't be empty")
+    for innerList in m_b:
+        if innerList == []:
+            raise TypeError("m_b can't be empty")
+    for innerList in m_a:
+        for item in innerList:
+            if not isinstance(item, (int, float)):
+                raise TypeError("m_a should contain only integers or floats")
+    for innerList in m_b:
+        for item in innerList:
+            if not isinstance(item, (int, float)):
+                raise TypeError("m_b should contain only integers or floats")
+    row = len(m_a[0])
+    for innerList in m_a:
+        if len(innerList) != row:
+            raise TypeError("each row of m_a must be of the same size")
+    for innerList in m_b:
+        if len(innerList) != row:
+            raise TypeError("each row of m_b must be of the same size")
+
     new_matrix = []
     for numList in m_a:
         current_matrix_list = [0] * len(m_b[0])

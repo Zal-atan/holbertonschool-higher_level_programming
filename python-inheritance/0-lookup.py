26c205922430a6b18d81fbeaeb182e309f07e801
Zal-atan <ethanzalta@yahoo.com>
2023-09-22 10:25:06 -0700

    Adding file 0-lookup.py

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# 0-lookup.py
+# Ethan Zalta
+"""This is a file creating a function which can look up available attributes
+and methods of an object."""
+
+def lookup(obj):
+    """Takes any object as input and returns list of all available
+    attributes and methods"""
+    return dir(obj)

cb75e482faf4e22d55bdf2228e6b3d41c667b51b
Zal-atan <ethanzalta@yahoo.com>
2023-09-23 11:06:23 -0700

    Adding file 5-save_to_json_file.py

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# 5-save_to_json_file.py
+# Ethan Zalta
+"""This is a file creating a function which takes an object, converts it to
+json, and then writes it to a file"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """This function makes an object(my_obj) into json format, then writes it
+    to a given filename"""
+    serialized = json.dumps(my_obj)
+    with open(filename, encoding="utf-8", mode="w") as file:
+        chars = file.write(serialized)
+        file.close
+    return chars

1e31b0d787afee31f88d8470dfe3456e15e3b168
Zal-atan <ethanzalta@yahoo.com>
2023-09-23 11:35:08 -0700

    Trying with json.dump instead of dumps

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -9,7 +9,6 @@ import json
 def save_to_json_file(my_obj, filename):
     """This function makes an object(my_obj) into json format, then writes it
     to a given filename"""
-    serialized = json.dumps(my_obj)
     with open(filename, encoding="utf-8", mode="w") as file:
-        chars = file.write(serialized)
-        file.close
+        json.dump(my_obj, file)
+        file.close()

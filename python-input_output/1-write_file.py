17af0c482a42fddd452a559c281a955689dd3921
Zal-atan <ethanzalta@yahoo.com>
2023-09-23 10:39:34 -0700

    Adding file 1-write_file

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# 0-read_file.py
+# Ethan Zalta
+"""This is a file creating a function which writes to a file"""
+
+
+def write_file(filename="", text=""):
+    """This function creats or overwrites a file with the given text"""
+    with open(filename, encoding="utf-8", mode="w") as file:
+        numChars = file.write(text)
+        file.close
+    return numChars

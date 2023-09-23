9d587669ec4e72edcbc45d9da3bd899002dcb781
Zal-atan <ethanzalta@yahoo.com>
2023-09-23 08:43:02 -0700

    Adding 5-base_geometry

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# 5-base_geometry.py
+# Ethan Zalta
+"""This is a file creating a class BaseGeometry"""
+
+
+class BaseGeometry:
+    """This is a class BaseGeometry"""
+    pass

d3b92c13f388a48b6587f878d46b9944abf468cb
Zal-atan <ethanzalta@yahoo.com>
2023-09-24 14:44:22 -0700

    Trying number 101-add_attribute

---
+++ python-inheritance/101-add_attribute.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# 101-add_attribute.py
+# Ethan Zalta
+"""This is a file creating a function to try to add new attributes to an
+object."""
+
+
+def add_attribute(obj, name, value):
+    listOfObjs = (int, float, str, list, tuple, dict, set, bool, complex)
+    if isinstance(obj, listOfObjs):
+        raise TypeError("can't add new attribute")
+    setattr(obj, name, value)

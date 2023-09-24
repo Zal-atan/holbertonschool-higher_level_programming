e3069b03d04dfbb63aa0c24d1c657688e576a778
Zal-atan <ethanzalta@yahoo.com>
2023-09-24 13:11:22 -0700

    First try on 10-student

---
+++ python-input_output/10-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+# 9-student.py
+# Ethan Zalta
+"""This is a file creating a class Student"""
+
+
+class Student:
+    """Creates Class Student, with public: first_name, last_name, and age:"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initializes instance with public: first_name, last_name, and age:"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Prints a json dictionary version of instance of student
+        if attrs is not None, only prints attrs in list attrs"""
+        if attrs is not None:
+            specific_attributes = {}
+            for item in attrs:
+                if item in self.__dict__:
+                    specific_attributes[item] = self.__dict__[item]
+            return specific_attributes
+        return self.__dict__

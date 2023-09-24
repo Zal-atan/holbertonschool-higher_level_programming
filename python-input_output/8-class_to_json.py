be78a3d307194bf2b634d73acfac9f40b1a5f2bf
Zal-atan <ethanzalta@yahoo.com>
2023-09-24 12:52:01 -0700

    Trying number 8-class_to_json

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# 8-class_to_json.pycp
+# Ethan Zalta
+"""This is a file creating a function which turns an instance of a class into
+a json version"""
+import json
+
+
+def class_to_json(obj):
+    """Takes input obj(instance of a class) and returns a json format
+    string of the information"""
+    return  obj.__dict__

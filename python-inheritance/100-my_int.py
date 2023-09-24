b17a624ab86ea148f33185bcb71997ee1846595d
Zal-atan <ethanzalta@yahoo.com>
2023-09-24 14:24:55 -0700

    Adding 100-my_int

---
+++ python-inheritance/100-my_int.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+# 100-my_int.py
+# Ethan Zalta
+"""This is a file creating a class myInt"""
+
+class MyInt(int):
+    """Class MyInt inherits from int"""
+
+    def __eq__(self, value):
+        if self.numerator == value:
+            return False
+        else:
+            return True
+
+    def __ne__(self, value):
+        if self.numerator == value:
+            return True
+        else:
+            return False

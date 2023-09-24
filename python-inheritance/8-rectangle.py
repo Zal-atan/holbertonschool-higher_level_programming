7a10b034051c7e558592fd1ba6bc288880d1d8f6
Zal-atan <ethanzalta@yahoo.com>
2023-09-24 14:08:14 -0700

    Trying to fix number 8

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -2,23 +2,7 @@
 # 8-rectangle.py
 # Ethan Zalta
 """This is a file creating a class BaseGeometry, then a subclass Rectangle"""
-
-
-class BaseGeometry:
-    """This is a class BaseGeometry"""
-
-    def area(self):
-        """Empty area function which raises exception"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """Validates if value is an integer, True if integer,
-        error if false"""
-        if type(value) is not int:
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
-        return True
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):

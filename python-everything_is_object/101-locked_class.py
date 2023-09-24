3e6535c8b4f08e854e5dc2924022e6643f8af4ef
Zal-atan <ethanzalta@yahoo.com>
2023-09-24 15:39:01 -0700

    Adding file 1010-locked_class

---
+++ python-everything_is_object/101-locked_class.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# 101-locked_class.py
+# Ethan Zalta
+"""This file creates a locked class called LockedClass"""
+
+
+class LockedClass:
+    """This class is locked from having any attributes except for first_name"""
+    def __init__(self):
+        self.first_name = None
+
+    def __setattr__(self, name, value):
+        if name != "first_name":
+            message = f"'LockedClass' object has no attribute '{name}'"
+            raise AttributeError(message)
+        super().__setattr__(name, value)

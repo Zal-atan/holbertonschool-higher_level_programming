0199cccba762715936c7d22fab83ee423a5ccca4
Zal-atan <ethanzalta@yahoo.com>
2023-09-24 15:42:19 -0700

    Trying 101 again without init

--- python-everything_is_object/101-locked_class.py
+++ python-everything_is_object/101-locked_class.py
@@ -6,8 +6,6 @@
 
 class LockedClass:
     """This class is locked from having any attributes except for first_name"""
-    def __init__(self):
-        self.first_name = None
 
     def __setattr__(self, name, value):
         if name != "first_name":

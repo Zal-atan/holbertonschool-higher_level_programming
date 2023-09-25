da606c43c28c7b4ade6c638e9e5b1a2663d31b2e
Zal-atan <ethanzalta@yahoo.com>
2023-09-25 09:15:02 -0700

    Trying a different way on 101, adding 103 both file answers

--- python-everything_is_object/101-locked_class.py
+++ python-everything_is_object/101-locked_class.py
@@ -7,8 +7,4 @@
 class LockedClass:
     """This class is locked from having any attributes except for first_name"""
 
-    def __setattr__(self, name, value):
-        if name != "first_name":
-            message = f"'LockedClass' object has no attribute '{name}'"
-            raise AttributeError(message)
-        super().__setattr__(name, value)
+    __slots__ = ("first_name")

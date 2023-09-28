8d3acb16dbda6428ab8018b901be93f0ffda6112
Zal-atan <ethanzalta@yahoo.com>
2023-09-28 14:37:03 -0700

    Adding size setter

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -12,5 +12,15 @@ class Square(Rectangle):
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
-        return(f"[Square] ({self.id}) {self.x}/{self.y} -"
-               f" {self.width}/{self.height}")
+        return(f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}")
+
+    @property
+    def size(self):
+        """returns the size of each side of the square"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        "Changes the width and height to be equal to size"
+        self.width = value
+        self.height = value
--- python-almost_a_circle/tests/test_square.py
+++ python-almost_a_circle/tests/test_square.py
@@ -201,35 +201,42 @@ class TestBase(unittest.TestCase):
 
     def test_print_str(self):
         s1 = Square(3, 2, 3, 10)
-        self.assertEqual(str(s1), "[Square] (10) 2/3 - 3/3")
+        self.assertEqual(str(s1), "[Square] (10) 2/3 - 3")
 
     def test_update_function_args(self):
         s1 = Square(10, 10, 10, 10)
-        self.assertEqual(str(s1), "[Square] (10) 10/10 - 10/10")
+        self.assertEqual(str(s1), "[Square] (10) 10/10 - 10")
         s1.update(89)
-        self.assertEqual(str(s1), "[Square] (89) 10/10 - 10/10")
+        self.assertEqual(str(s1), "[Square] (89) 10/10 - 10")
         s1.update(89, 2)
-        self.assertEqual(str(s1), "[Square] (89) 10/10 - 2/10")
+        self.assertEqual(str(s1), "[Square] (89) 10/10 - 2")
         s1.update(89, 2, 3)
-        self.assertEqual(str(s1), "[Square] (89) 10/10 - 2/3")
+        self.assertEqual(str(s1), "[Square] (89) 10/10 - 2")
         s1.update(89, 2, 3, 4)
-        self.assertEqual(str(s1), "[Square] (89) 4/10 - 2/3")
+        self.assertEqual(str(s1), "[Square] (89) 4/10 - 2")
         s1.update(89, 2, 3, 4, 5)
-        self.assertEqual(str(s1), "[Square] (89) 4/5 - 2/3")
+        self.assertEqual(str(s1), "[Square] (89) 4/5 - 2")
 
     def test_update_function_kwargs(self):
         s1 = Square(10, 10, 10, 10)
-        self.assertEqual(str(s1), "[Square] (10) 10/10 - 10/10")
+        self.assertEqual(str(s1), "[Square] (10) 10/10 - 10")
         s1.update(height=1)
-        self.assertEqual(str(s1), "[Square] (10) 10/10 - 10/1")
+        self.assertEqual(str(s1), "[Square] (10) 10/10 - 10")
         s1.update(width=1, x=2)
-        self.assertEqual(str(s1), "[Square] (10) 2/10 - 1/1")
+        self.assertEqual(str(s1), "[Square] (10) 2/10 - 1")
         s1.update(y=1, width=2, x=3, id=89)
-        self.assertEqual(str(s1), "[Square] (89) 3/1 - 2/1")
+        self.assertEqual(str(s1), "[Square] (89) 3/1 - 2")
         s1.update(x=1, height=2, y=3, width=4)
-        self.assertEqual(str(s1), "[Square] (89) 1/3 - 4/2")
+        self.assertEqual(str(s1), "[Square] (89) 1/3 - 4")
 
     def test_update_function_incorrect(self):
         s1 = Square(10, 10, 10, 10)
         with self.assertRaises(KeyError):
             s1.update(1, 2, 3, 4, 5, 6)
+
+    def test_size_setter(self):
+        s1 = Square(10, 10, 10, 10)
+        self.assertEqual(s1.width, 10)
+        s1.size = 7
+        self.assertEqual(s1.width, 7)
+        self.assertEqual(s1.height, 7)

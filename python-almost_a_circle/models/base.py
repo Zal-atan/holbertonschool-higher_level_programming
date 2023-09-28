ee5b3db886825ce211048fe00b0e46b7f71df7f8
Zal-atan <ethanzalta@yahoo.com>
2023-09-28 09:04:43 -0700

    Adding new directory python-almost_a_circle, README, tests folder and models folder

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+# base.py
+# Ethan Zalta
+"""This is a file creating a Base Class"""
+
+
+class Base:
+    """Base Class"""
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """ Initalizing Base Class"""
+        if id != None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects

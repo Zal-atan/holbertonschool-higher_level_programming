de053d68ccf5c76f5d635a954ebac45b69eb8f36
Zal-atan <ethanzalta@yahoo.com>
2023-09-29 08:17:35 -0700

    Trying a quick change

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -20,6 +20,6 @@ class Base:
 
     def to_json_string(list_dictionaries):
         """Turns a list of dictionaries into a jsons tring"""
-        if list_dictionaries is None or not list_dictionaries:
-            return []
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
         return json.dumps(list_dictionaries)

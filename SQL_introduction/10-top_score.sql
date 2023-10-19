eb41de00757b66ee5d22b72cb6d41a35b07f0385
Zal-atan <109318907+Zal-atan@users.noreply.github.com>
2023-10-19 13:48:57 -0500

    Trying all caps on 10

--- SQL_introduction/10-top_score.sql
+++ SQL_introduction/10-top_score.sql
@@ -1,2 +1,2 @@
 -- List only score and name from second_table in descending score order
-Select score, name from second_table order by score desc;
+SELECT score, name FROM second_table ORDER BY score DESC;

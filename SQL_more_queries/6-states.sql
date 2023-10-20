0d2549b584e313309b70e28abd5d5283e7181909
Zal-atan <109318907+Zal-atan@users.noreply.github.com>
2023-10-20 12:01:09 -0500

    Adding file 6-states.sql

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,9 @@
+-- Create table 'states' in database 'hbtn_0d_usa' where attributes
+-- id INT unique, auto generated, can’t be null and is a primary key
+-- name VARCHAR(256) can’t be null
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (
+    id INT UNIQUE AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(256) NOT NULL
+);

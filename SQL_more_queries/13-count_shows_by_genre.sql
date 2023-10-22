b99f04ebecb4a063a9e7d0a368fcde100bd6ea88
Zal-Atan <ethanzalta@gmail.com>
2023-10-22 10:15:34 -0500

    Fixing name to genre

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- List all genres and number of shows of that type of genre, in descending order
 -- of number of shows per genre
-SELECT tv_genres.name, count(*) as number_of_shows
+SELECT tv_genres.name AS genre, count(*) as number_of_shows
 FROM tv_genres
 INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 GROUP BY tv_genres.name

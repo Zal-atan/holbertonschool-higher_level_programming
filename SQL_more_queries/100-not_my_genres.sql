863dc80d4c35beeeaa2fd158ae85c2f2ade84262
Zal-Atan <ethanzalta@gmail.com>
2023-10-22 11:05:13 -0500

    Adding file 100-not_my_genres.sql

---
+++ SQL_more_queries/100-not_my_genres.sql
@@ -0,0 +1,11 @@
+-- List all genres not associated with show "Dexter"
+SELECT DISTINCT name FROM tv_genres
+INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+INNER JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
+WHERE name NOT IN (
+    SELECT name FROM tv_genres
+    INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+    INNER JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
+    WHERE tv_shows.title = "Dexter"
+)
+ORDER BY name;

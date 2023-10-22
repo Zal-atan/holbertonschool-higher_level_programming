bca6da6ea9bee786e3c0e7ab14eac9e62e469b83
Zal-Atan <ethanzalta@gmail.com>
2023-10-22 10:21:09 -0500

    Trying another small change for 13

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,6 @@
--- List all genres and number of shows of that type of genre, in descending order
--- of number of shows per genre
-SELECT tv_genres.name AS genre, count(*) as number_of_shows
+-- List all genres and number of shows of that type of genre, in descending
+-- order of number of shows per genre
+SELECT tv_genres.name AS genre, count(tv_genres.id) as number_of_shows
     FROM tv_genres
     INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
     GROUP BY genre

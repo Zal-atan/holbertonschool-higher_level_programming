0c5926533d5952f6ac2213be342d1a08ab061b8f
Zal-Atan <ethanzalta@gmail.com>
2023-10-22 10:26:41 -0500

    Forgot to capatalize as

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,7 +1,7 @@
 -- List all genres and number of shows of that type of genre, in descending order of number of shows per genre
 -- order of number of shows per genre
-SELECT tv_genres.name AS genre, count(*) as number_of_shows
+SELECT tv_genres.name AS genre, count(*) AS number_of_shows
     FROM tv_genres
-    JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+    INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
     GROUP BY genre
     ORDER BY number_of_shows DESC;

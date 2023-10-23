f18a0df610fbd6ddf95d3f6ecaf2720d283b8669
Zal-Atan <ethanzalta@gmail.com>
2023-10-23 16:42:05 -0500

    Trying to take out all extraneous things on 0

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,17 +1,13 @@
 #!/usr/bin/python3
-# 0-select_states.py
-# Ethan Zalta
 """ This script lists all the states from database 'hbtn_0e_0_usa'
 Inputs are mysql username, mysql password and database name"""
 import MySQLdb
-from sys import argv
+import sys
 
 
 if __name__ == "__main__":
-    """Shows all the states from the table in database"""
-    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                         passwd=argv[2], database=argv[3])
-
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], database=sys.argv[3])
     c = db.cursor()
 
     c.execute("SELECT * FROM states ORDER BY id ASC")

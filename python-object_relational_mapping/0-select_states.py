7df4752bf3d9144d7e48b77cbdb5d025cb7a6ccc
Zal-Atan <ethanzalta@gmail.com>
2023-10-23 16:47:35 -0500

    Changing c to cursor

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -8,10 +8,13 @@ import sys
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                          passwd=sys.argv[2], database=sys.argv[3])
-    c = db.cursor()
+    cursor = db.cursor()
 
-    c.execute("SELECT * FROM states ORDER BY id ASC")
-    all_data = c.fetchall()
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    all_data = cursor.fetchall()
 
     for row in all_data:
         print(row)
+
+    cursor.close()
+    db.close()

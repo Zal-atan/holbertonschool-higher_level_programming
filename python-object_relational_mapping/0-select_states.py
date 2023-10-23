16f0266c643019b5fa80b506f0b3467201447e40
Zal-Atan <ethanzalta@gmail.com>
2023-10-23 16:58:56 -0500

    Taking out order by in 0

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,20 +1,24 @@
 #!/usr/bin/python3
+# 0-select_states.py
+# Ethan Zalta
 """ This script lists all the states from database 'hbtn_0e_0_usa'
 Inputs are mysql username, mysql password and database name"""
 import MySQLdb
-import sys
+from sys import argv
 
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                         passwd=sys.argv[2], database=sys.argv[3])
-    cursor = db.cursor()
+    """Shows all the states from the table in database"""
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], database=argv[3])
 
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
-    all_data = cursor.fetchall()
+    c = db.cursor()
+
+    c.execute("SELECT * FROM states")
+    all_data = c.fetchall()
 
     for row in all_data:
         print(row)
 
-    cursor.close()
+    c.close()
     db.close()

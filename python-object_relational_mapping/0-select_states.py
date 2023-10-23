93afc38f6d29cc6a54c61ddd93b3deb85cba7ae9
Zal-Atan <ethanzalta@gmail.com>
2023-10-23 17:05:16 -0500

    Trying again on -

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,20 +1,15 @@
 #!/usr/bin/python3
-# 0-select_states.py
-# Ethan Zalta
 """ This script lists all the states from database 'hbtn_0e_0_usa'
 Inputs are mysql username, mysql password and database name"""
 import MySQLdb
 from sys import argv
 
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+if __name__ == '__main__':
+    db = MySQLdb.connect(host='localhost', port=3306, user=argv[1],
                          passwd=argv[2], database=argv[3])
-
-    c = db.cursor()
-
-    c.execute("SELECT * FROM states")
-    all_data = c.fetchall()
-
+    cursor = db.cursor()
+    cursor.execute('SELECT * FROM states')
+    all_data = cursor.fetchall()
     for row in all_data:
         print(row)

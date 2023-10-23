17af422c30ba95976d124f56f0941e35e6e88de9
Zal-Atan <ethanzalta@gmail.com>
2023-10-23 16:23:51 -0500

    Trying again

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -4,13 +4,13 @@
 """ This script lists all the states from database 'hbtn_0e_0_usa'
 Inputs are mysql username, mysql password and database name"""
 import MySQLdb
-import sys
+from sys import argv
 
 
 if __name__ == "__main__":
     """Shows all the states from the table in database"""
-    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                         passwd=sys.argv[2], database=sys.argv[3])
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], database=argv[3])
 
     c = db.cursor()
 
@@ -19,6 +19,3 @@ if __name__ == "__main__":
 
     for row in all_data:
         print(row)
-
-    c.close()
-    db.close()

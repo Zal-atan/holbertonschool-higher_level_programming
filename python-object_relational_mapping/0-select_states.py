bb3341be599f5a56a26b2015eab2f066fd78929a
Zal-Atan <ethanzalta@gmail.com>
2023-10-23 16:14:40 -0500

    Fixing pycodestyle

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -3,15 +3,14 @@
 # Ethan Zalta
 """ This script lists all the states from database 'hbtn_0e_0_usa'
 Inputs are mysql username, mysql password and database name"""
-
 import MySQLdb
 from sys import argv
 
 
 if __name__ == "__main__":
     """Shows all the states from the table in database"""
-    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2],
-                 database=argv[3])
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], database=argv[3])
 
     c = db.cursor()
 

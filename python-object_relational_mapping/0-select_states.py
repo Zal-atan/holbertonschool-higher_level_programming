f23f1bd4f449c659765b1406c822cc2f83f0b38f
Zal-Atan <ethanzalta@gmail.com>
2023-10-23 16:10:52 -0500

    Trying small changes again

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -10,18 +10,16 @@ from sys import argv
 
 if __name__ == "__main__":
     """Shows all the states from the table in database"""
-    # connect to database
     db = connect(host="localhost", port=3306, user=argv[1], passwd=argv[2],
                  database=argv[3])
 
-    # cursor object
     c = db.cursor()
 
-    c.execute("SELECT * FROM states ORDER BY id")
+    c.execute("SELECT * FROM states ORDER BY id ASC")
     all_data = c.fetchall()
 
     for row in all_data:
         print(row)
 
-    c.close()  # close cursor
-    db.close()  # close database connection
+    c.close()
+    db.close()

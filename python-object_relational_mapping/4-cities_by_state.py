5e025f3c2dbb73aea5d8d78b214df978e9249c62
Zal-Atan <ethanzalta@gmail.com>
2023-10-23 17:58:27 -0500

    Adding file 4-cities_by_state.py

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+# 4-cities_by_state.py
+# Ethan Zalta
+"""Lists all cities in order of id and their attached state"""
+import MySQLdb
+from sys import argv
+
+
+def show_city_state():
+    """Shows all cities in order of id and the state attatched"""
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], database=argv[3])
+    c = db.cursor()
+
+    c.execute("SELECT cities.id, cities.name, states.name FROM cities"
+              "INNER JOIN states ON states.id = cities.state_id"
+              "ORDER BY id ASC")
+    all_data = c.fetchall()
+
+    for state in all_data:
+        if state[1][0] == 'N':
+            print(state)
+
+    c.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    show_city_state()

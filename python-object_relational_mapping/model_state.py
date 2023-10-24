08e077691a5f71fdea2933ded641efc7567639e1
Zal-atan <109318907+Zal-atan@users.noreply.github.com>
2023-10-24 13:56:59 -0500

    Fixing pycodestyle

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -7,6 +7,7 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class State(Base):
     """Class 'State' which will inherit behaviors from Base"""
     __tablename__ = 'states'

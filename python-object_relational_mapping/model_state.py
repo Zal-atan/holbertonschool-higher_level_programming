b2ff6b541df7a1bde0b2bb5237a71e682b9cd461
Zal-atan <109318907+Zal-atan@users.noreply.github.com>
2023-10-24 13:54:28 -0500

    Adding file for task 6 model_state.py

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -2,3 +2,13 @@
 # model_state.py
 # Ethan Zalta
 """Class definition of a 'state', instantiates from Base = declarative_base"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class State(Base):
+    """Class 'State' which will inherit behaviors from Base"""
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)

1c35a7c96d06f0857577d637e97fd0f8fbe02572
Zal-atan <109318907+Zal-atan@users.noreply.github.com>
2023-10-24 14:29:41 -0500

    adding / for shebang

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,4 +1,11 @@
-#!usr/bin/python3
+#!/usr/bin/python3
 # 7-model_state_fetch_all.py
 # Ethan Zalta
 """ List all states from database in order of id"""
+import sys
+from model_state import Base, State
+from sqlalchemy import sessionmaker
+from sqlalchemy import (create_engine)
+
+def all_states():
+    engine = create_engine()

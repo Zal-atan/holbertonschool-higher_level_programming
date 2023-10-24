45546894abfd54f4cfb858ae0b5369ee0a936e1b
Zal-atan <109318907+Zal-atan@users.noreply.github.com>
2023-10-24 15:51:12 -0500

    Adding base of 12

---
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+# 12-model_state_update_id_2.py
+# Ethan Zalta
+""" Updates state number 2 to New Mexico"""
+from sys import argv
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import (create_engine)
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    new_state = State(name="Louisiana")
+    session.add(new_state)
+    session.commit()
+
+    print(new_state.id)

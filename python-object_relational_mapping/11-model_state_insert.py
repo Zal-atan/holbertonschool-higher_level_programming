2bd6b1aec30d4a1501511808b46b32a5ca535428
Zal-atan <109318907+Zal-atan@users.noreply.github.com>
2023-10-24 15:44:11 -0500

    Adding file 1model_state_insert.py

---
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+# 11-model_state_insert.py
+# Ethan Zalta
+""" Add a new state and print the new id number"""
+from sys import argv
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import (create_engine)
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    new_state = State(name="Louisiana")
+    session.add(new_state)
+    session.commit()
+
+    print(new_state.id)

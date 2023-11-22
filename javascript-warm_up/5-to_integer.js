303c24fee07e757931cb6ae6d109f444252f3f88
Zal-atan <109318907+Zal-atan@users.noreply.github.com>
2023-11-22 13:28:33 -0600

    Semi-standard file 5

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 const args = process.argv;
 if (!args[2]) {
-  console.log("Not a number")
+  console.log('Not a number');
 } else {
   if ((Number(args[2]))) {
-    console.log(`My number: ${Math.floor(Number(args[2]))}`)
+    console.log(`My number: ${Math.floor(Number(args[2]))}`);
   } else {
-    console.log("Not a number")
+    console.log('Not a number');
   }
 }

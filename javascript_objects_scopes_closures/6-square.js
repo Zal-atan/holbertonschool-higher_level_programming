4e4697ea758667dddf76595c6043f55ebecafe18
Zal-Atan <ethanzalta@gmail.com>
2023-11-27 09:59:48 -0600

    Fixing semi-standard

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -4,7 +4,6 @@ const Sq = require('./5-square.js');
 
 // Extends class Square
 class Square extends Sq {
-
   // Print a square using char c, or "X" if undefined
   charPrint (c = 'X') {
     for (let i = 1; i <= this.height; i++) {

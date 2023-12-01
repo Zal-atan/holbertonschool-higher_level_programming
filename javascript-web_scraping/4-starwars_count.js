22d5232a6cab1a0161b01d42f9cb920bfc25cfef
Zal-atan <109318907+Zal-atan@users.noreply.github.com>
2023-12-01 15:32:35 -0600

    finished parsing number of movies for each char

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -6,5 +6,17 @@ const request = require('request');
 
 request(process.argv[2], function (err, response, body) {
   if (err) throw err;
-  console.log(JSON.parse(body).results);
+  let counter = 0;
+  const fullJson = (JSON.parse(body).results);
+
+  // Iterate over list of Jsons
+  for (const movie in fullJson) {
+    const allChars = fullJson[movie].characters;
+    for (const char in allChars) {
+      if (char === 18) {
+        counter++;
+      }
+    }
+  }
+  console.log(counter);
 });

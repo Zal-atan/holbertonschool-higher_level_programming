997053cbe4b5ad34affa461c4f3ee8b32b912ed4
Zal-atan <109318907+Zal-atan@users.noreply.github.com>
2023-12-01 15:14:30 -0600

    Added .title to narrow it down

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -2,9 +2,9 @@
 // This program displays the displays a star wars name from a number input {argument 1]}
 
 const request = require('request');
-const website = "https://swapi-api.hbtn.io/api/films/" + process.argv[2];
+const website = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 
 request(website, function (err, response, body) {
   if (err) throw err;
-  console.log(JSON.parse(body));
+  console.log(JSON.parse(body).title);
 });

6b9ac618e820698ce380ab4cfddd9b04382de757
Zal-atan <109318907+Zal-atan@users.noreply.github.com>
2023-12-01 15:39:01 -0600

    took out a print statement

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -13,7 +13,7 @@ request(process.argv[2], function (err, response, body) {
   for (const movie in fullJson) {
     const allChars = fullJson[movie].characters;
     for (const char in allChars) {
-      console.log(allChars[char]);
+      // console.log(allChars[char]);
       if (allChars[char].includes('/18/')) {
         counter++;
       }

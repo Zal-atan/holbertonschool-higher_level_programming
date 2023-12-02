48f09179aa148b86d2888f439153a56cdacad996
Zal-atan <109318907+Zal-atan@users.noreply.github.com>
2023-12-01 18:49:15 -0600

    Adding file 6

---
+++ javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+// computes number of tasks computed by each user_id
+// website given as {argument 1]}
+
+const request = require('request');
+
+request(process.argv[2], function (err, response, body) {
+  if (err) throw err;
+  const allJobs = (JSON.parse(body));
+  // console.log(allJobs)
+  let userObj = {}
+  for (task in allJobs) {
+    // console.log(allJobs[task])
+    if (allJobs[task].completed === true) {
+      userId = allJobs[task].userId
+      const hasKey = userId in userObj;
+      if (hasKey) {
+        userObj[userId] += 1;
+      } else {
+        userObj[userId] = 1;
+      }
+    }
+  }
+  console.log(userObj)
+});

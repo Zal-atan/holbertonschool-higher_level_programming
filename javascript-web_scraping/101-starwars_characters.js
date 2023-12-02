15a5ec65788f6248163335538c802ed34d1e645b
Zal-Atan <ethanzalta@gmail.com>
2023-12-02 12:23:24 -0600

    Changing the order of characters on 101

--- javascript-web_scraping/101-starwars_characters.js
+++ javascript-web_scraping/101-starwars_characters.js
@@ -2,18 +2,23 @@
 // This program displays all of the characters from Star wars movie number {argument 1]}
 
 const request = require('request');
-const website = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+const website = 'https://swapi-api.hbtn.io/api/films/';
 
 request(website, function (err, response, body) {
   if (err) throw err;
-  // Get each characters website
-  const characters = JSON.parse(body).characters;
-  // For Each website
-  characters.forEach(function (char) {
-    request(char, function (err, response, body) {
-      if (err) throw err;
-      // Print character name
-      console.log(JSON.parse(body).name);
-    });
-  });
+  const movies = JSON.parse(body).results;
+  movies.forEach(function (film) {
+    if (film.episode_id == process.argv[2]) {
+      // Get each characters website
+      const characters = film.characters;
+      // // For Each website
+      characters.forEach(function (char) {
+        request(char, function (err, response, body) {
+          if (err) throw err;
+          // Print character name
+          console.log(JSON.parse(body).name);
+        });
+      });
+    }
+  })
 });
